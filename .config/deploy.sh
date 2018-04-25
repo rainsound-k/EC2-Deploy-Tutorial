5dc69b87423eb25921679783ef60f4b5ef3df472
rainsound-k <rainsound128@gmail.com>
2018-04-25 18:32:12 +0900

    deploy.sh 수정

--- .config/deploy.sh
+++ .config/deploy.sh
@@ -3,7 +3,10 @@ sudo rm -rf /etc/nginx/sites-enabled/*
 sudo cp -f /srv/ec2-deploy/.config/nginx-app.conf /etc/nginx/sites-available/nginx-app.conf
 sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
 sudo cp -f /srv/ec2-deploy/.config/uwsgi.service /etc/systemd/system/uwsgi.service
-/home/ubuntu/.pyenv/versions/study-ec2-deploy/bin/python /srv/ec2-deploy/app/manage.py collectstatic --noinput
+cd /srv/ec2-deploy/app
+/bin/bash -c \
+'/home/ubuntu/.pyenv/versions/study-ec2-deploy/bin/python \
+/srv/ec2-deploy/app/manage.py collectstatic --noinput' ubuntu
 sudo systemctl enable uwsgi
 sudo systemctl daemon-reload
 sudo systemctl restart uwsgi nginx
